965cac81f29baa01309f75003836138d1e9b7e79
Thiago Santa Clara <strovsk@outlook.com>
2024-05-16 16:19:28 -0300

    refactor: prettier formatter

--- frontend/src/components/ProjetRouanetSlider.jsx
+++ frontend/src/components/ProjetRouanetSlider.jsx
@@ -9,40 +9,42 @@ import 'swiper/css/scrollbar';
 
 import ProjetoRouanetCard from './ProjetoRouanetCard';
 
-
 function ProjetoRouanetSlider(projects) {
-    const slideSize = 290;
-    const slideGap = 25;
-    const numberOfSlidesPerView = 3;
+  const slideSize = 290;
+  const slideGap = 25;
+  const numberOfSlidesPerView = 3;
 
-    return (
+  return (
     <div style={{ width: (slideSize + slideGap) * numberOfSlidesPerView }}>
-        <Swiper
-        modules={[ Navigation, Pagination ]}
+      <Swiper
+        modules={[Navigation, Pagination]}
         spaceBetween={slideGap}
         slidesPerView={numberOfSlidesPerView}
         navigation
         pagination={{ clickable: true }}
-        >
-            {projects.map(project => (
-                <SwiperSlide key={project.id_projeto}>
-                    <ProjetoRouanetCard project={project} />
-                </SwiperSlide>
-            ))}
-        </Swiper>
-  </div>)
+      >
+        {projects.map(project => (
+          <SwiperSlide key={project.id_projeto}>
+            <ProjetoRouanetCard project={project} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
 
 ProjetoRouanetSlider.propTypes = {
-    projects: PropTypes.arrayOf(PropTypes.shape({
-        id_projeto: PropTypes.number.isRequired,
-        nome: PropTypes.string.isRequired,
-        resumo: PropTypes.string.isRequired,
-        municipio: PropTypes.string.isRequired,
-        uf: PropTypes.string.isRequired,
-        valor_aprovado: PropTypes.string.isRequired,
-        valor_captado: PropTypes.string.isRequired,
-    })).isRequired,
+  projects: PropTypes.arrayOf(
+    PropTypes.shape({
+      id_projeto: PropTypes.number.isRequired,
+      nome: PropTypes.string.isRequired,
+      resumo: PropTypes.string.isRequired,
+      municipio: PropTypes.string.isRequired,
+      uf: PropTypes.string.isRequired,
+      valor_aprovado: PropTypes.string.isRequired,
+      valor_captado: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default ProjetoRouanetSlider;
