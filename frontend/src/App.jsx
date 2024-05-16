942e7b74f1027e4327ed6e2324267acf5dbde8c7
Thiago Santa Clara <strovsk@outlook.com>
2024-05-16 12:51:53 -0300

    chore: Refactor App component and improve project display
    Remove unused imports and separate the screen in two sections for apresentation and test results

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import { useState, useEffect } from 'react';
 import { projetosRouanet } from './services/projetosRouanet';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 
 import Typography from '@mui/material/Typography';
 
 import ProjetoRouanetCard from './components/ProjetoRouanetCard';
 import bacgkround from './assets/background.svg';
-import './styles/App.css';
-import './styles/typography.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -26,29 +24,49 @@ function App() {
 
   return (
     <>
-      <Typography variant="h4" component="div" align='left' color="#459ca8">
-          Ver outros projetos do propoente
-      </Typography>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={10}
-        slidesPerView={3}
-        style={{
-          "--swiper-navigation-color": "#000",
-          "--swiper-navigation-size": "25px",
-        }}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-      >
-        {projects.map(project => (
-          <SwiperSlide key={project.id}>
-            <ProjetoRouanetCard  project={project} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <section style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
+        <div>
+          <Typography variant="h1" component="div" align='left' color="#459ca8">
+              Case técnico da Simbi
+          </Typography>
+          <Typography variant='body2' component="div" align='left' color="#459ca8">
+              Candidato: <strong>Thiago Santa Clara Pereira</strong>
+          </Typography>
+          <Typography variant='body2' component="div" align='left' color="#459ca8">
+              Data de início: <strong>15/05/2024</strong>
+          </Typography>
+          <Typography variant='body2' component="div" align='left' color="#459ca8">
+              Data de entrega: <strong></strong>
+          </Typography>
+        </div>
+        <img src={bacgkround} alt="case técnico da Simbi" />
+      </section>
 
-      <img src={bacgkround} alt="case técnico da Simbi" />
+      <section style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
+        <div style={{ background: '#fff', padding: 40, borderRadius: 10 }}>
+          <Typography variant="h5" component="div" align='left' color="#459ca8" gutterBottom>
+              Ver outros projetos do propoente
+          </Typography>
+          <div style={{ width: (290 + 25) * 3, height: 500 }}>
+            <Swiper
+              modules={[ Navigation, Pagination ]}
+              spaceBetween={25}
+              slidesPerView={3}
+              navigation
+              pagination={{ clickable: true }}
+            >
+                {projects.map(project => (
+                  <SwiperSlide key={project.id_projeto}>
+                    <ProjetoRouanetCard project={project} />
+                  </SwiperSlide>
+                ))}
+            </Swiper>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Button>+ VER TODOS</Button>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
